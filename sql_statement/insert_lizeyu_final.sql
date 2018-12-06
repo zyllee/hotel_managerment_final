acc61994bc19d105d4ac6afd0404a67254d59e66
Esrappelt <1090822794@qq.com>
2018-12-06 13:34:30 +0800

    添加插入语句

---
+++ sql_statement/insert_lizeyu_final.sql
@@ -0,0 +1,12 @@
+select * from room
+select * from roomTypeAndPrice
+insert into roomTypeAndPrice values('单人间',32);
+insert into room values('101','单人间','五星级');
+
+
+select * from customers
+select * from usertab
+
+insert into usertab values('肖誉杰','160510111');
+insert into usertab values('李泽宇','160510114');
+insert into usertab values('王虎','160510113');
