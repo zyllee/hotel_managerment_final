3b492e62a930286f1026387300380e2dbf3b354c
Esrappelt <1090822794@qq.com>
2018-12-06 22:02:14 +0800

    修改登录页面

--- WebRoot/js/login.js
+++ WebRoot/js/login.js
@@ -1,18 +1,13 @@
 $(function() {
   var $login = $(".login-button");
+  var timer;
   $login.on('click', function(event) {
-    addClass(document.querySelector(".login"), "active");
-    setTimeout(function() {
-      addClass(document.querySelector(".sk-rotating-plane"), "active")
-      document.querySelector(".login").style.display = "none";
-    }, 800);
     login();
   });
-
+  
   function login() {
     var $user = $("input[name=user]").val();
     var $pwd = $("input[name=pwd]").val();
-    console.log($user + $pwd);
     $.ajax({
         url: 'loginServlet',
         type: 'post',
@@ -24,45 +19,18 @@ $(function() {
       })
       .done(function(data) {
         if($.trim(data) == "false") {
-          console.log('失败');
-          window.location = "index.jsp";
+          clearInterval(timer);
+          $(".login").show();
+          $(".login").removeClass('active');
+          $(".sk-rotating-plane").removeClass('active')
+          $(".error").stop().fadeIn(2000).fadeOut(2000);
           return;
         }
         console.log(data);
-        success();
+        window.location = "welcome.jsp";
       })
       .fail(function(e) {
         console.log(e.status);
       })
-  }
-
-  function success() {
-    removeClass(document.querySelector(".login"), "active");
-    removeClass(document.querySelector(".sk-rotating-plane"), "active");
-    document.querySelector(".login").style.display = "block";
-    console.log('成功');
-    window.location = "welcome.jsp";
-  }
-
-  function hasClass(elem, cls) {
-    cls = cls || '';
-    if (cls.replace(/\s/g, '').length == 0) return false; 
-    return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
-  }
-
-  function addClass(ele, cls) {
-    if (!hasClass(ele, cls)) {
-      ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
-    }
-  }
-
-  function removeClass(ele, cls) {
-    if (hasClass(ele, cls)) {
-      var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
-      while (newClass.indexOf(' ' + cls + ' ') >= 0) {
-        newClass = newClass.replace(' ' + cls + ' ', ' ');
-      }
-      ele.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
   }
 });
