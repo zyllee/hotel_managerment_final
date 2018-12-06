7a41f314be0db9302a522d260c5ecf96d7310b2b
Esrappelt <1090822794@qq.com>
2018-12-06 14:20:34 +0800

    修改Readme

---
+++ WebRoot/js/login.js
@@ -0,0 +1,64 @@
+$(function() {
+  var $login = $(".login-button");
+  $login.on('click', function(event) {
+    addClass(document.querySelector(".login"), "active");
+    setTimeout(function() {
+      addClass(document.querySelector(".sk-rotating-plane"), "active")
+      document.querySelector(".login").style.display = "none";
+    }, 800);
+    login();
+  });
+
+  function login() {
+    var $user = $("input[type=user]").val();
+    var $pwd = $("input[type=pwd]").val();
+    $.ajax({
+        url: 'loginServlet',
+        type: 'post',
+        dataType: 'text',
+        data: {
+          'user': $user,
+          'pwd': $pwd
+        }
+      })
+      .done(function(data) {
+        if($.trim(data) == "false") {
+          alert("登录失败!");
+          return;
+        }
+        success();
+      })
+      .fail(function(e) {
+        console.log(e.status);
+      })
+  }
+
+  function success() {
+    removeClass(document.querySelector(".login"), "active");
+    removeClass(document.querySelector(".sk-rotating-plane"), "active");
+    document.querySelector(".login").style.display = "block";
+    alert("登录成功");
+  }
+
+  function hasClass(elem, cls) {
+    cls = cls || '';
+    if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
+    return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
+  }
+
+  function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) {
+      ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
+    }
+  }
+
+  function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+      var newClass = ' ' + ele.className.replace(/[\t\r\n]/g, '') + ' ';
+      while (newClass.indexOf(' ' + cls + ' ') >= 0) {
+        newClass = newClass.replace(' ' + cls + ' ', ' ');
+      }
+      ele.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+  }
+});
